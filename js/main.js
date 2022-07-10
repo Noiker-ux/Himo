f4c6ca8b52660773ffd1ff2c17c59765c938c1f4
Noiker-ux <71761947+Noiker-ux@users.noreply.github.com>
2022-07-11 00:06:33 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+    $('.bike-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: true,
+        fade: true
+      });
+
+      $('.slider__items').slick({
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: true,
+        fade: true
+      });
+});
